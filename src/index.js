1f2e7643b8f6f2af6c4a25a30c714c1be446428f
yanjianliao <yanjianliao@ryous-MacBook-Pro.local>
2018-06-19 23:27:27 -0700

    First

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+
+    <div className="container-fluid">
+        <Mainpage/>
+    </div>,
+
+    document.getElementById('root')
+);
