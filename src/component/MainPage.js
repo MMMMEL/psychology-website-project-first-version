edb47d62b011e8fa41e7f2297cf96df1665372a1
yanjianliao <yanjianliao@ryous-MacBook-Pro.local>
2018-06-19 23:30:26 -0700

    Just for test

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-export default class Mainpage extends React.Component{
+export default class MainPage extends React.Component{
     render() {
         return (
             <h1>hello</h1>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import MainPage from "./component/MainPage";
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
-ReactDOM.render(
+ReactDOM
+
+.render(
 
     <div className="container-fluid">
-        <Mainpage/>
+        <MainPage/>
     </div>,
 
     document.getElementById('root')
